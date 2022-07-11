203e8a8dcb8ba2936e2d7a2a6c23cafd853fb38d
Colby Ludwig <me@colbyludwig.com>
2022-07-10 22:55:16 -0600

    add CORS to worker, support tracks w/o timestamp

--- workers/last-fm/src/index.js
+++ workers/last-fm/src/index.js
@@ -26,7 +26,7 @@ function parseTrackObject(track) {
     artist: track.artist["#text"],
     album: track.album["#text"],
     artworkUrl: track.image[0]["#text"],
-    playedAt: track.date["uts"],
+    playedAt: (track.date && track.date["uts"]) || "currently playing",
   };
 }
 
@@ -34,9 +34,41 @@ function parseTrackObject(track) {
 // and return them as formatted JSON objects.
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
+    // Respond to pre-flight CORS requests early.
+    const { headers } = request;
+
+    // Handle OPTIONS requests early for CORS.
+    if (request.method === "OPTIONS") {
+      console.log("HANDLING OPTIONS REQUEST");
+      if (
+        headers.get("Origin") !== null &&
+        headers.get("Access-Control-Request-Method") !== null &&
+        headers.get("Access-Control-Request-Headers") !== null
+      ) {
+        // Allow all methods & headers through.
+        return new Response(null, {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+            "Access-Control-Max-Age": "86400",
+            "Access-Control-Allow-Headers": request.headers.get(
+              "Access-Control-Request-Headers"
+            ),
+          },
+        });
+      } else {
+        console.log("HANDLING OPTIONS REQUEST");
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+          headers: {
+            Allow: "GET, HEAD, POST, OPTIONS",
+          },
+        });
+      }
+    }
 
     // Ensure we 404 any unrelated requests to prevent against unnecessary cache hits & CPU time.
+    const url = new URL(request.url);
     if (url.pathname !== "/") {
       return new Response(JSON.stringify({ error: "not found" }), {
         status: 404,
@@ -59,12 +91,24 @@ export default {
 
       // Finally, return the tracks as JSON!
       return new Response(JSON.stringify(tracks), {
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+          "Access-Control-Max-Age": "86400",
+        },
       });
     } catch (err) {
       // Something weird happened -- return a generic error message and log the full details.
       console.error(err);
-      return new Response("error fetching from lastfm", { status: 500 });
+      return new Response("error fetching from lastfm", {
+        status: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+          "Access-Control-Max-Age": "86400",
+        },
+      });
     }
   },
 };
