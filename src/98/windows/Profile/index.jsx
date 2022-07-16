d8e36dc57a4dbaf18854bb391433a8359bbaf666
Colby Ludwig <me@colbyludwig.com>
2022-07-15 22:15:20 -0600

    refactor profile buttons, add github link

--- src/98/windows/Profile/index.jsx
+++ src/98/windows/Profile/index.jsx
@@ -4,35 +4,34 @@ import { useDesktop } from "../../context/desktop";
 
 import "./index.css";
 
-function getLinkForButton(id) {
-  switch (id) {
-    case "linkedin":
-      return `https://linkedin.com/in/colbyludwig`;
-    case "twitter":
-      return `https://twitter.com/cdl`;
-    case "dribbble":
-      return `https://dribbble.com/ludwig`;
-    case "email":
-      return `mailto:me@colbyludwig.com`;
-  }
-}
+const PROFILE_LINKS = {
+  linkedin: { label: "LinkedIn", value: `https://linkedin.com/in/colbyludwig` },
+  twitter: { label: "Twitter", value: `https://twitter.com/cdl` },
+  github: { label: "Github", value: `https://github.com/cdl` },
+  email: { label: "Email", value: `mailto:me@colbyludwig.com` },
+};
 
 export default function Profile(props) {
+  const profileButtons = Object.values(PROFILE_LINKS).map(
+    ({ label, value }, key) => (
+      <button
+        key={key}
+        className="profile__btn"
+        data-link={value}
+        onClick={handleClick}
+      >
+        {label}
+      </button>
+    )
+  );
+
   function handleClick(ev) {
     ev.preventDefault();
-    const id = ev.target?.id;
-    const url = getLinkForButton(id);
+    const url = ev.target?.getAttribute("data-link");
 
-    if (!id || !url) return;
+    console.log(url, ev.target);
 
-    // Track with Google Analytics if it's available.
-    if (window.gtag && typeof window.gtag === "function") {
-      window.gtag("event", "click_link", {
-        id,
-        url,
-        label: ev.target.innerText,
-      });
-    }
+    if (!url) return;
 
     window.open(url, "_blank");
   }
@@ -52,20 +51,7 @@ export default function Profile(props) {
         <br />
         Based in Edmonton, AB.
       </p>
-      <div className="button-group profile__btn-group">
-        <button className="profile__btn" onClick={handleClick} id="linkedin">
-          LinkedIn
-        </button>
-        <button className="profile__btn" onClick={handleClick} id="twitter">
-          Twitter
-        </button>
-        <button className="profile__btn" onClick={handleClick} id="dribbble">
-          Dribbble
-        </button>
-        <button className="profile__btn" onClick={handleClick} id="email">
-          Email
-        </button>
-      </div>
+      <div className="button-group profile__btn-group">{profileButtons}</div>
     </Window>
   );
 }
