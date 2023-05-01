87a23a7273b02951702b6090100b4ccca0c4c71f
Colby Ludwig <me@colbyludwig.com>
2023-04-30 22:52:46 -0600

    add PlausibleProvider for analytics to app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Script from "next/script";
 import "../src/styles.css";
 import "98.css/dist/98.css";
+import PlausibleProvider from "next-plausible";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <PlausibleProvider
+        domain="colbyludwig.com"
+        customDomain="https://a.ldwg.me"
+      >
+        <Component {...pageProps} />
+      </PlausibleProvider>
+    </>
+  );
 }
